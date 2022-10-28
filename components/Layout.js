b2253b5957a15cf3c89c556d0d81e5f7f5e18782
Sango <60422984+MatyilaSango@users.noreply.github.com>
2022-10-28 23:55:33 +0200

    changed background

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,5 @@
 import Navbar from './Navbar'
-import styled from 'styled-components'
 import styles from '../styles/Home.module.css'
-
-const MainBody = styled.div`
-    width: 1080px;
-    
-`;
 
 export default function Layout({ children }){
     return(
