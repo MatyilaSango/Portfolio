07a04283483648c63a9135eecf77b114c243b9ed
Sango <60422984+MatyilaSango@users.noreply.github.com>
2022-10-29 09:04:27 +0200

    edited home

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,13 @@ export default function Home() {
           <h2>About me 😊 -------</h2>
           <p>
             My name is <b><NameSpan>Sango Matyila</NameSpan></b>. I am a computer science graduate from University of the Western Cape.
-            I have a passion for programming. I like to code whether it be creating applications or websites.
+            I have a passion for programming. I like to code whether it be creating applications or websites. I've been involved in some
+            number of projects; In creation of a library system and a tutoring web application. 
             <br/><br/>
             To be a developer at SovTec would be a dream come true. Working with world-class expert team of developers would be a 
             greate opportunity for me to learn new skills and to learn from my seniors how this industry works and how to create beautiful, functional software.
             <br/><br/>
-            One day I want to start my own company. To work with skilled developers at SovTech will enable that dream to come true.
+            One day I want to create a software to will change the lives of everyone. To work with skilled developers at SovTech will enable that dream to come true.
           </p>
         </div>
 
